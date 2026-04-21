0a4e13565c2a3360681b71649b26bca4d614adc2
Anderson <anderson.araujo.work@gmail.com>
2026-04-21 16:42:24 -0300

    feat: setup inicial — Spring Boot, Docker Compose, Dockerfile

---
+++ src/main/java/br/com/gastrovision/api/GastrovisionApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gastrovision.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastrovisionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastrovisionApiApplication.class, args);
+	}
+
+}
